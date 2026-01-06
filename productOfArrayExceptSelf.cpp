4766a70c5b31408933218abd54c4c037e34431fd
SUJAY GIJRE <sujaygijre@gmail.com>
2026-01-06 07:30:52 -0500

    Update productOfArrayExceptSelf.cpp

--- productOfArrayExceptSelf.cpp
+++ productOfArrayExceptSelf.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
-        vector<int> answer;
-        map<int, vector<int>> um;
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> left(n,1), right(n, 1), answer(n, 1);
 
-        for (int i=0;i<mat.size();i++) {
-            for (int j=0;j<mat[0].size();j++) {
-                um[(i+j)].push_back(mat[i][j]);
-            }
+        left[0] = 1;
+        for (int i=1;i<n;i++) {
+            left[i] = left[i-1]*nums[i-1];
         }
-        int dir = 0;
-        for (auto itr: um) {
-            if (dir%2 == 0) 
-                reverse(itr.second.begin(), itr.second.end());
-            for (auto x: itr.second) {
-                answer.push_back(x);
-            }
-            dir++;
+
+        right[n-1]=1;
+        for (int j=n-2;j>=0;j--) {
+            right[j] = right[j+1]*nums[j+1];
         }
+
+        for (int i=0;i<n;i++)
+            answer[i] = left[i]*right[i];
 
         return answer;
     }
